918b176e79af885c8edd4c6d11efe616b75c9340
bellieny <daniel.bellieny@gmail.com>
2024-02-20 17:06:51 -0800

    functional shell SGE submission

--- sh/nidali_provisional_run.sh
+++ sh/nidali_provisional_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -l h_rt=00:10:00
+#$ -l h_rt=00:50:00
 #$ -j y
 #$ -l mem_free=1G
 #$ -l scratch=2G
@@ -18,40 +18,35 @@ export OMPI_MCA_pml=ucx
 date
 hostname
 
+# Adjust this varible prior to running the qsub
+# Don't use trailing dash here
+PROJECT_ROOT_PATH=""
+
 # Set the base directories and query name
 QUERY_NAME="DRA2A"
-TARGET_DB_BASE="/wynton/home/doudna/bellieny-rabelo/projects/nidali/pyoon_bug/batch_"
-QUERY_PATH="/wynton/home/doudna/bellieny-rabelo/projects/nidali/pyoon_bug/query_DAT"
-OUTPUT_BASE="/wynton/home/doudna/bellieny-rabelo/projects/nidali/pyoon_bug/nidali_search_output"
-
-
-echo $QUERY_NAME
-echo $TARGET_DB_BASE
-echo $QUERY_PATH
-echo $OUTPUT_BASE
-
+TARGET_DB_BASE=$PROJECT_ROOT_PATH/"batch_"
+QUERY_PATH=$PROJECT_ROOT_PATH/"query_DAT"
+OUTPUT_BASE=$PROJECT_ROOT_PATH/"search_output"
 
 OUTPUT_DIR="${OUTPUT_BASE}/${QUERY_NAME}"
-echo output_dir -> $OUTPUT_DIR
+echo output_dir: $OUTPUT_DIR
 
 # Use SGE_TASK_ID to iterate over the batches
-# BATCH_INDEX=$((SGE_TASK_ID-1))
-BATCH_INDEX=0
-echo $BATCH_INDEX
+BATCH_INDEX=$((SGE_TASK_ID-1))
 
 # Create the output directory if it doesn't exist
 mkdir -p "$OUTPUT_DIR"
 
 # Define the target database path for the current task
 TARGET_DB_PATH="${TARGET_DB_BASE}${BATCH_INDEX}"
-echo target_db_path $TARGET_DB_PATH
+
 
 # Save the current directory
 CURRENT_DIR=$(pwd)
 
 # Change to the target database directory
 cd $TARGET_DB_PATH
-echo go to $TARGET_DB_PATH
+
 
 ln -s $QUERY_PATH query_dat
 
@@ -59,7 +54,7 @@ ln -s $QUERY_PATH query_dat
 LIST_FILE="list_batch_${BATCH_INDEX}.txt"
 ls *.dat | sed 's/\.dat$//' > "$LIST_FILE"
 
-echo START NIDALI.PM RUN WITH:\n query: $QUERY_NAME\n list of files: $LIST_FILE\n query path: $QUERY_PATH\n target_db: $TARGET_DB_PATH\n
+echo START NIDALI.PM RUN WITH: query: $QUERY_NAME list of files: $LIST_FILE query path: $QUERY_PATH target_db: $TARGET_DB_PATH
 
 # Run the DaliLite analysis
 /wynton/home/doudna/bellieny-rabelo/projects/nidali/DaliLite.v5/bin/dali.pl --cd1 "$QUERY_NAME" --db "$LIST_FILE" --dat1 query_dat --dat2 $TARGET_DB_PATH --oneway --outfmt "summary"
