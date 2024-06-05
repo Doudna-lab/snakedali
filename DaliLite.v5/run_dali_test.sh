b5841daf528ccf909a39a06ba0b18f4d6994ff5d
dbellieny <daniel.bellieny@gmail.com>
2024-06-05 14:52:55 -0700

    remove legacy files

--- DaliLite.v5/run_dali_test.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-#$ -S /bin/bash  # run job as a Bash shell [IMPORTANT]
-#$ -cwd          # run job in the current working directory
-
-./test.csh
-
--- DaliLite.v5/test/run_makeblast_db.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-#$ -S /bin/bash  # run job as a Bash shell [IMPORTANT]
-#$ -cwd          # run job in the current working directory
-
-module load CBI
-module load blast
-makeblastdb  -in pdb.seq -out pdb.blast -dbtype prot
